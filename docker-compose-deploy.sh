886d6c770e4ad24497b90a30adb93f38699d34fb
Timothy Chung <timchunght@gmail.com>
2020-10-16 12:46:39 -0400

    update docker-compose script to use seed

--- docker-compose-deploy.sh
+++ docker-compose-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker login -u="${VRT_DOCKER_USER}" -p="${VRT_DOCKER_PASSWORD}" quay.io
 export COMMIT_ID="${VRT_VERSION}" && docker-compose -f docker/docker-compose.yml build nginx-proxy
+export COMMIT_ID="${VRT_VERSION}" && docker-compose -f docker/docker-compose.yml run api-service /go/bin/api seed
 export COMMIT_ID="${VRT_VERSION}" && docker-compose -f docker/docker-compose.yml run api-service /go/bin/api migrate up
 export COMMIT_ID="${VRT_VERSION}" && docker-compose -f docker/docker-compose.yml up -d
