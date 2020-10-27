85caf5248f95405b4d6005dc33935c369441c04b
Timothy Chung <timchunght@gmail.com>
2020-10-27 15:44:28 -0400

    update k8 seed config

--- kubernetes/deploy_k8.sh
+++ kubernetes/deploy_k8.sh
@@ -6,7 +6,8 @@ kubectl apply -f generated/vrtuoso/templates/cluster-setup/cloud-generic.yaml;
 kubectl apply -f generated/vrtuoso/templates/cluster-setup/do_nginx-ingress-controller.yaml;
 
 kubectl delete -f generated/vrtuoso/templates/migrations;
-kubectl apply -f generated/vrtuoso/templates/migrations;
+kubectl apply -f generated/vrtuoso/templates/migrations/api-service-migration.yaml;
+kubectl apply -f generated/vrtuoso/templates/migrations/api-service-seed.yaml;
 kubectl apply -f generated/vrtuoso/templates/secrets;
 kubectl apply -f generated/vrtuoso/templates/deployments;
 kubectl apply -f generated/vrtuoso/templates/svcs;
